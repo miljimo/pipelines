6db5c99a74bce3a772856b85abea003a5b46c00d
johnson obaro <johnson.obaro@hotmail.com>
2019-11-30 19:37:06 +0000

    Concurrency Pipe Implemented

--- pipeline/pipe.py
+++ pipeline/pipe.py
@@ -1,21 +1,196 @@
-from events import BaseObject;
+from events import EventHandler, Event, BaseObject;
+from threading import Thread, Lock;
+import queue;
+
+THREAD_WAIT_TIME  =  100/1000;
+
+class IdleEvent(Event):
+
+    def __init__(self, sender):
+        super().__init__("idle.event");
+        self.__Sender  = sender;
+
+    @property
+    def Sender(self):
+        return self.__Sender;
+
+class ProcessingEvent(Event):
+
+    def __init__(self,sender,  data):
+        super().__init__("processing.event");
+        self.__Data      = data;
+        self.__Sender    = sender;
+
+    @property
+    def Data(self):
+        return self.__Data;
+
+    @property
+    def Sender(self):
+        return self.__Sender;
+
+
 
 class Pipe(BaseObject):
 
-    def __init__(self, pipeName):
+    def __init__(self, **kwargs):
         super().__init__();
-        self.Name  =  pipeName;
-        pass;
+        self.__LinkedPipe = None;
+        name  = kwargs['name'] if('name' in kwargs) else '';
+        link  = kwargs['link'] if('link' in kwargs) else None;
+        if(isinstance(link, Pipe)):
+           self.__LinkedPipe  = link;
+           
+        self.Name                 = name;
+        self.__ProcessThread      = None;
+        self.__ProcessLockThread  =  Lock();
+        self.__IsProcessing       = False;
+        self.__IdleHandler        = EventHandler();
+        self.__ProcessedHandler   = EventHandler();
+        self.__Jobs               = Pipe.__JobCollection();
 
-    def Process(self, data):
-        self.__Data  =  data;
-        result  =  None;
-        if(self.__Data != None):
-            result = data + 1;           
-        return result
+    @property
+    def ProcessedHandler(self):
+        return self.__ProcessedHandler;
 
+    @ProcessedHandler.setter
+    def ProcessedHandler(self, handler):
+        if(isinstance(handler , EventHandler) != True):
+            raise TypeError("ProcessedHandler : expecting a EventHandler");
+        self.__ProcessedHandler  =  handler;
+    
+    @property
+    def Jobs(self):
+        return self.__Jobs;
+
+    @property
+    def LinkedPipe(self):
+        return self.__LinkedPipe;
+
+    @LinkedPipe.setter
+    def LinkedPipe(self, pipe):
+        if(isinstance(pipe, Pipe)):
+            if(self.IsProcessing):
+                raise ValueError("Unable to set Linked Pipe when the current pipe is running");
+            self.__LinkedPipe = pipe;
+    
+    @property
+    def IdleHandler(self):
+        return self.__IdleHandler;
+
+    @IdleHandler.setter
+    def IdleHandler(self, handler):
+        if(isinstance(handler, EventHandler) != True):
+            raise TypeError("@IdleHandler: expecting an eventhandler type");
+        self.__IdleHandler = handler;        
+
+    def Start(self):
+        self.__ProcessLockThread.acquire();
+        if(self.IsProcessing != True):
+            self.__ProcessThread  =  Thread(target= self.__ThreadLoop);
+            self.__ProcessThread.daemon = True;
+            self.__ProcessThread.start();
+            
+
+    def __ThreadLoop(self):
+        self.__IsProcessing  = True;
+        self.__ProcessLockThread.release();
+        while(self.IsProcessing):
+            #Process job queue
+            if(self.Jobs.IsEmpty != True):                
+                data  = self.Jobs.Front;
+                if(data != None):
+                    self._OnProcess(data);
+            else:
+                # Idle stage for the pipe
+                if(self.IdleHandler != None):
+                    self.IdleHandler(IdleEvent(self));
+                    
+    @property
+    def IsProcessing(self):
+        return self.__IsProcessing;
+
+    @IsProcessing.setter
+    def IsProcessing(self, value):
+        if(type(value) == bool):
+            if(self.IsProcessing != value):
+                if(value == True):
+                    self.Start(); # Start the process
+                else:
+                    self.Stop(); # Stop the process
+                
+    def Stop(self):
+        self.__ProcessLockThread.acquire();
+        self.__IsProcessing  = False;
+        if(self.__ProcessThread != None):
+            self.__ProcessThread.join(THREAD_WAIT_TIME);
+            self.__ProcessThread   = None;
+
+        self.__ProcessLockThread.release();
+
+    def _OnProcess(self, data):
+        if(self.LinkedPipe != None):
+            self.LinkedPipe.Jobs.Add(data);
+        if(self.ProcessedHandler != None):
+            self.ProcessedHandler(ProcessingEvent(self, data));
+
+
+    class __JobCollection(object):
+
+        def __init__(self):
+            self.__JobLocker  = Lock();
+            self.__Queue      = queue.Queue()
+            self.__JobList    = list();
+
+        def Add(self, data):
+            self.__JobLocker.acquire();
+            self.__Queue.put(data);       
+            self.__JobLocker.release();
+        
+        @property
+        def IsEmpty(self):
+            return (self.Count == 0);
+
+        @property
+        def Count(self):
+            return self.__Queue.qsize();
+        
+        @property
+        def Front(self):
+            self.__JobLocker.acquire();
+            result  = None;
+            if(self.Count > 0):
+                result  = self.__Queue.get();
+            self.__Queue.task_done();  
+            self.__JobLocker.release();
+            return result;
+
+            
+
+
+                
 
 if(__name__ == "__main__"):
-    pipe  =  Pipe("Simple");
-    result = pipe.Process(5);
-    print(result);
+    def OnProcessed(event):
+        print("Stage 1 ={0}\n".format(event.Data));
+    def OnDataRecieved(e):
+        print("Stage 2 ={0}\n".format(e.Data));
+        
+    pipe  =  Pipe(name  =  "Simple");
+    pipe2  =  Pipe(name  =  "Simple2");
+    pipe.ProcessedHandler += OnProcessed;
+    pipe.LinkedPipe  = pipe2;
+    pipe2.ProcessedHandler +=OnDataRecieved;
+    pipe.Start();
+    pipe2.Start();
+    Counter = 0;
+    while(pipe.IsProcessing):
+        pipe.Jobs.Add(Counter);
+        Counter +=1;
+       
+        if(Counter == 20):
+            print("\nDone\n");
+            pipe.Stop();
+            pipe2.Stop();
+        
+
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,19 +10,9 @@ PIP_EVENT_COMPLETED        =  0x02;
 
 class PipeLine(Pipe):
 
-    def __init__(self, **kwargs):        
-        self.__ReusePipe     = False;
-        self.__IsProcessing =  False;
-        #inputs
-        reuse_pipes = kwargs['keep_pipes'] if('keep_pipes' in kwargs) else False;
-        name   = kwargs['name'] if(('name' in kwargs) and (type(kwargs['name']) == str)) else "pipeline";
-        super().__init__(name); # Call the parent constructor
-        
-        if(type(reuse_pipes) == bool):
-            self.__ReusePipe  =  reuse_pipes;
-            
-        self.__Pipes  =  list();
-        self.__PipeProcessed  =  EventHandler();
+    def __init__(self, **kwargs):
+        self.__Pipes           =  list();
+        super().__init__(**kwargs);
         self.__Completed      =  EventHandler();
         self.__ErrorOccured   =  EventHandler();
         
@@ -34,16 +24,6 @@ class PipeLine(Pipe):
     def ErrorOccured(self, handler):
         if(isinstance(handler, EventHandler)):
             self.__ErrorOccured  = handler;
-      
-    @property
-    def IsProcessing(self):
-        return self.__IsProcessing;
-    
-    @IsProcessing.setter
-    def IsProcessing(self, status):
-        if(type(status)  == bool):
-            self.__IsProcessing = status;
-
     @property
     def Completed(self):
         return self.__Completed;
@@ -54,34 +34,24 @@ class PipeLine(Pipe):
     def Completed(self, handler):
         if(isinstance(handler, EventHandler)):
             self.__Completed =  handler;
-    
-    @property
-    def PipeProcessed(self):
-        return self.__PipeProcessed;
-
-    @PipeProcessed.setter
-    def PipeProcessed(self, handler):
-        if(isinstance(handler, EventHandler)):
-               self.__PipeProcessed = handler;
+  
     
     def Pipe(self, pipe):
         if(isinstance(pipe, Pipe)!= True):
             raise TypeError("Expecting a pipe object but {0} given".format(type(pipe)));
         if(self.IsExists(pipe)):
             return ;
+        if(self.Count ==0):
+             self.LinkedPipe = pipe;
+        else:
+            index = (self.Count  -1);
+            temp = self.Pipes[index];
+            temp.LinkedPipe  = pipe;
         self.Pipes.append(pipe);  
         return self;
 
-    @property
-    def Front(self):
-        data  =  None;
-        if(len(self.Pipes) >0):
-            data  = self.Pipes.pop(0);
-        return data;
-
     def IsExists(self, pipe):
         status  =  True;
-        
         if(isinstance(pipe, Pipe)):
             status  =  False;
             for tpipe in self.Pipes:
@@ -94,61 +64,56 @@ class PipeLine(Pipe):
     def Pipes(self):
         return self.__Pipes;
 
-    def Process(self, data):
-        try:
-            if(data != None):
-                result =  None;
-                processing_data  =  copy.deepcopy(data);
-                self.__ReuseList =  list();
-                IsProcessing     = True;
-                while((len(self.Pipes) != 0) and (IsProcessing  == True)):    
-                    pipe  =  self.Front;                
-                    process_result  =  pipe.Process(processing_data);
-                    if(process_result == None):
-                        break;
-                    processing_data = process_result;                
-                    if(self.__ReusePipe):
-                        self.__ReuseList.append(pipe);
-                    self.__Processed(pipe, processing_data);
-                    
-                if(processing_data  != None):
-                    result      = processing_data ;                
-                self.__Pipes    = self.__ReuseList;
-                self.__ProcessCompletedEvent(result);
-        except Exception as err:
-            if(self.ErrorOccured != None):
-                self.ErrorOccured(ErrorEvent("@Processing: ", err));
-            else:
-                raise err;
-           
-    def __ProcessCompletedEvent(self, result):
-        if(isinstance(self.Completed, EventHandler)):
-            self.Completed(PipeEvent(self, result,PIP_EVENT_COMPLETED ));
-            
-    def __Processed(self, pipe,  result):
-        if(self.PipeProcessed != None):           
-            event  =  PipeEvent(pipe, result, PIP_EVENT_PROCESSED_STATE);
-            self.PipeProcessed(event);
+    def Start(self):
+        # Track the last processing stage
+        if(self.Count > 0):
+            self.__LastPipe  =  self.Pipes[self.Count -1];
+        for pipe in self.Pipes:
+            pipe.ProcessedHandler +=self.__DataProcessingInProgress
+            pipe.Start();
+        super().Start();
+
+    def Stop(self):
+        for pipe in self.Pipes:
+            pipe.ProcessedHandler -= self.__DataProcessingInProgress;
+            pipe.Stop();
+        super().Stop();
+        
+    @property
+    def Count(self):
+        return len(self.Pipes);
+
+    def __DataProcessingInProgress(self, event):
+        if(event.Sender == self.__LastPipe):            
+            if(self.Completed != None):
+                self.Completed(event);
+        else:
+           if(self.ProcessedHandler != None):
+               self.ProcessedHandler(event);
+       
 
 
 if(__name__ =="__main__"):
-    class MulPipe(Pipe):
-        def Process(self , data):
-            return data * 2;
+    def OnHandler(event):
+        print(event.Sender.Name);
+        
     pipeline  =  PipeLine(keep_pipes  = True);
-    pipeline.Pipe(Pipe("Placing the order")
-                  ).Pipe(Pipe("Validating the order")
-                  ).Pipe(Pipe("Processing the order")
-                  ).Pipe(MulPipe("Preparing the other re"));
+    pipeline.Pipe(Pipe(name ="Start Counter"));
+    pipeline.Pipe(Pipe(name ="Mulit Counter"));
+    pipeline.Pipe(Pipe(name ="Transformer Counter"));
+    pipeline.Completed += OnHandler;
     
-    def OnPipeProcessed(event):
-         print("Pipe{0}, data ={1}".format(event.Pipe.Name, event.Result));
-    def OnCompleted(event):
-        print("Final result  {0}".format(event.Result));
-    pipeline.PipeProcessed += OnPipeProcessed;
-    pipeline.Completed     +=  OnCompleted;
-    start  = 1;
-    start  =  pipeline.Process(start);
+    pipeline.Start();
+    count  = 10;
+    try: 
+        while(pipeline.IsProcessing):
+            pipeline.Jobs.Add(count);
+            count +=1;
+          
+            pass;
+    except:
+        pipeline.Stop();
+        print("Completed");
 
         
         
